247c1ffbe7b63ace268b4e663e6ba7488e69ecd9
FuzeDaBomb <wolfiewhite332@gmail.com>
2025-12-22 16:50:04 +0800

    fix

--- server.js
+++ server.js
@@ -47,3 +47,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/products', async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM norsbakery ORDER BY id ASC");
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).send("Server Error");
+    }
+});
